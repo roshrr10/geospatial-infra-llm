98afc1294b99a2ccda7fcf1a3fc191b9b0149b52
roshrr10 <roshanraju7654@gmail.com>
2026-03-27 21:34:21 +0530

    Add frontend, remove venv, add .gitignore

--- venv/Lib/site-packages/maplibre/projection.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ProjectionType(Enum):
-    MERCATOR = "mercator"
-    VERTICAL_PERSPECTIVE = "vertical-perspective"
-    GLOBE = "vertical-perspective"
--- venv/Lib/site-packages/maplibre/shiny/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mapcontext import MapContext
-from .renderer import render_maplibregl
-from .ui import output_maplibregl
